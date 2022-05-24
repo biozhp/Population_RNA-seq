7d9b75247fbe0c0bad5e60cbf62027afc56b0907
Peng Zhao <51162669+biozhp@users.noreply.github.com>
2022-05-24 19:28:12 +0800

    Update 02.SNP imputation and annotation.sh

--- 02.SNP imputation and annotation.sh
+++ 02.SNP imputation and annotation.sh
@@ -3,8 +3,8 @@
 ## Northwest A&F University
 ## This script briefly summarizes the codes in the analysis process, for reference only.
 
-## SNP imputation (https://pcingola.github.io/SnpEff/se_inputoutput/)
+## SNP imputation
 java -jar beagle.18May20.d20.jar gt=snp.vcf out=snp.impute.vcf nthreads=8
-## SNP Annotation
+## SNP Annotation (https://pcingola.github.io/SnpEff/se_inputoutput/)
 java -jar snpEff.jar build -gff3 -v wheat
 java -jar snpEff.jar ann wheat snp.vcf > ann.vcf
